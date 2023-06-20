20a2dfcefec1f58a82908935a190627ca2946ca8
tartir <faresyousef2@gmail.com>
2023-06-20 23:56:40 +0300

    ASSi2

--- app/src/main/java/com/example/tarter/AddQ.java
+++ app/src/main/java/com/example/tarter/AddQ.java
@@ -1,47 +1,34 @@
 package com.example.tarter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AddQ {
-    ArrayList<QA> QAns=new ArrayList<>();
+    private List<QA> QAns;
+
     public AddQ() {
-        QAns.add(new QA("What is the highest mountain peak in the world?","Mount Everest"));
-        QAns.add(new QA("Which river is the longest in the world?","The Nile"));
-        QAns.add(new QA("What is the capital city of Australia?","Canberra"));
-        QAns.add(new QA("What is the largest desert in the world?","The Sahara Desert"));
-        QAns.add(new QA("What is the name of the largest ocean on Earth?","The Pacific Ocean"));
-        QAns.add(new QA("Which continent is the largest by land area?","Asia"));
-        QAns.add(new QA("What is the name of the largest lake in Africa?","Lake Victoria"));
-        QAns.add(new QA("What is the name of the river that runs through London?","The River Thames"));
+        QAns = new ArrayList<>();
+        QAns.add(new QA("What is the highest mountain peak in the world?", "Mount Everest"));
+        QAns.add(new QA("Which river is the longest in the world?", "The Nile"));
+        QAns.add(new QA("What is the capital city of Australia?", "Canberra"));
+        QAns.add(new QA("What is the largest desert in the world?", "The Sahara Desert"));
+        QAns.add(new QA("What is the name of the largest ocean on Earth?", "The Pacific Ocean"));
+        QAns.add(new QA("Which continent is the largest by land area?", "Asia"));
+        QAns.add(new QA("What is the name of the largest lake in Africa?", "Lake Victoria"));
+        QAns.add(new QA("What is the name of the river that runs through London?", "The River Thames"));
     }
 
-    public int[] random3(){
-        int[] random3=new int[3];
+    public QA[] getRandomQuestions(int count) {
         Random rand = new Random();
-        int randomNum = rand.nextInt(8);
-        random3[0]=randomNum;
-        int randomNum2 = rand.nextInt(8);
-        while(randomNum2==randomNum){
-            randomNum2 = rand.nextInt(8);
+        List<QA> randomQuestions = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            int randomIndex = rand.nextInt(QAns.size());
+            randomQuestions.add(QAns.get(randomIndex));
+            QAns.remove(randomIndex);
         }
-        random3[1]=randomNum2;
-        int randomNum3 = rand.nextInt(8);
-        while(randomNum3==randomNum||randomNum3==randomNum2){
-            randomNum3 = rand.nextInt(8);
-        }
-        random3[2]=randomNum3;
-        return random3;
+
+        return randomQuestions.toArray(new QA[0]);
     }
-
-    public QA[] GetAnswer(){
-        int[] random3=random3();
-        QA[] QAARR=new QA[3];
-        QAARR[0]=QAns.get(random3[0]);
-        QAARR[1]=QAns.get(random3[1]);
-        QAARR[2]=QAns.get(random3[2]);
-
-        return QAARR;
-    }
-
 }
--- app/src/main/java/com/example/tarter/MainActivity.java
+++ app/src/main/java/com/example/tarter/MainActivity.java
@@ -1,40 +1,50 @@
 package com.example.tarter;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.helper.widget.MotionEffect;
-
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv1 ;
-    TextView tv2 ;
-    TextView tv3 ;
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
 
-    Spinner sp1;
-    Spinner sp2;
-    Spinner sp3;
+    private Spinner sp1;
+    private Spinner sp2;
+    private Spinner sp3;
 
-    Button b;
-    Button b2;
+    private Button b;
+    private Button b2;
 
+    private Animation fadeInAnimation;
+
+    private List<QA> qalist;
+    private RecyclerView recyclerView;
+    private QAAdapter adapter;
+
+    private SharedPreferences sharedPreferences;
+    private static final String PREFS_NAME = "MyPrefs";
+    private static final String KEY_QUESTION1 = "question1";
+    private static final String KEY_QUESTION2 = "question2";
+    private static final String KEY_QUESTION3 = "question3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        QA[] QAARR;
-
-        AddQ qa = new AddQ();
-
-        QAARR = qa.GetAnswer();
 
         tv1 = findViewById(R.id.question1);
         tv2 = findViewById(R.id.question2);
@@ -44,67 +54,78 @@ public class MainActivity extends AppCompatActivity {
         sp2 = findViewById(R.id.answer2_spinner);
         sp3 = findViewById(R.id.answer3_spinner);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.number, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp1.setAdapter(adapter);
-
-        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.number, android.R.layout.simple_spinner_item);
-        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp2.setAdapter(adapter2);
-
-        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.number, android.R.layout.simple_spinner_item);
-        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp3.setAdapter(adapter3);
-
-
         b = findViewById(R.id.answer1);
         b2 = findViewById(R.id.answer2);
 
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        qalist = new ArrayList<>();
+        adapter = new QAAdapter(qalist);
+        recyclerView.setAdapter(adapter);
+
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        loadData();
+
+        b.setOnClickListener(e -> {
+            String answer0 = sp1.getSelectedItem().toString();
+            String answer1 = sp2.getSelectedItem().toString();
+            String answer2 = sp3.getSelectedItem().toString();
+
+            if ((answer0.equals(qalist.get(0).getAns()) && answer1.equals(qalist.get(1).getAns()))
+                    || (answer0.equals(qalist.get(0).getAns()) && answer2.equals(qalist.get(2).getAns()))
+                    || (answer1.equals(qalist.get(1).getAns()) && answer2.equals(qalist.get(2).getAns()))) {
+                Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        b2.setOnClickListener(e -> {
+            loadData();
+        });
+
+        fadeInAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+        tv1.startAnimation(fadeInAnimation);
+        tv2.startAnimation(fadeInAnimation);
+        tv3.startAnimation(fadeInAnimation);
+    }
+
+    private void loadData() {
+        AddQ addQ = new AddQ();
+        QA[] QAARR = addQ.getRandomQuestions(3);
+
+        qalist.clear();
+        qalist.add(new QA(QAARR[0].getQuestion(), QAARR[0].getAns()));
+        qalist.add(new QA(QAARR[1].getQuestion(), QAARR[1].getAns()));
+        qalist.add(new QA(QAARR[2].getQuestion(), QAARR[2].getAns()));
+
+        adapter.notifyDataSetChanged();
 
         tv1.setText(QAARR[0].getQuestion());
         tv2.setText(QAARR[1].getQuestion());
         tv3.setText(QAARR[2].getQuestion());
 
-        sp1.setPrompt(QAARR[0].getAns());
-        sp2.setPrompt(QAARR[1].getAns());
-        sp3.setPrompt(QAARR[2].getAns());
+        ArrayAdapter<String> answer1Adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        answer1Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp1.setAdapter(answer1Adapter);
 
-        b.setOnClickListener(e -> {
+        ArrayAdapter<String> answer2Adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        answer2Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp2.setAdapter(answer2Adapter);
 
-            String answer0 = sp1.getSelectedItem().toString();
-            String answer1 = sp2.getSelectedItem().toString();
-            String answer2 = sp3.getSelectedItem().toString();
-
-
-            if ((answer0.equals(QAARR[0].getAns()) && answer1.equals(QAARR[1].getAns())) || (answer0.equals(QAARR[0].getAns()) && answer2.equals(QAARR[2].getAns())) || (answer1.equals(QAARR[1].getAns()) && answer2.equals(QAARR[2].getAns()))) {
-                Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(MainActivity.this, "Incorrect", Toast.LENGTH_SHORT).show();
-            }
-
-        });
+        ArrayAdapter<String> answer3Adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+        answer3Adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp3.setAdapter(answer3Adapter);
 
 
-
-        b2.setOnClickListener(e -> {
-
-            AddQ qa1 = new AddQ();
-            QA[] QAARR1;
-
-            QAARR1 = qa1.GetAnswer();
-
-
-            tv1.setText(QAARR1[0].getQuestion());
-            tv2.setText(QAARR1[1].getQuestion());
-            tv3.setText(QAARR1[2].getQuestion());
-
-            sp1.setPrompt(QAARR1[0].getAns());
-            sp2.setPrompt(QAARR1[1].getAns());
-            sp3.setPrompt(QAARR1[2].getAns());
-
-
-
-        });
-
-      }
+        for (QA qa : qalist) {
+            answer1Adapter.add(qa.getAns());
+            answer2Adapter.add(qa.getAns());
+            answer3Adapter.add(qa.getAns());
+        }
     }
+
+
+}
